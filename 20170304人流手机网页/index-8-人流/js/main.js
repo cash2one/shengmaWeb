d115f3e2345ea75d968778dc467d89d93afab0e7
Sup_ <1099982811@qq.com>
2017-03-27 11:22:41 +0800

    Add files via upload

---
+++ 20170304人流手机网页/index-8-人流/js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+	//解决启动界面的二次渲染
+	(function(){
+		$('body').css('visibility','visible');
+	})();
+	
+	//服务
+	(function(){
+		var menuLi=$('#fuwu').find('.menu').find('li');
+		var mainLi=$('#fuwu').find('.main').find('li');
+		var iNow=0;
+		menuLi.mouseenter(function(){
+			menuLi.removeClass('native');
+			$(this).addClass('native');
+			iNow=$(this).index();
+			mainLi.removeClass('native');
+			mainLi.eq(iNow).addClass('native');
+		})				
+	})();
+
+	//时间
+	(function(){
+		var menuLi=$('#shijian').find('ul').find('li');
+		menuLi.mouseenter(function(){
+			menuLi.removeClass('native');
+			$(this).addClass('native');
+			iNow=$(this).index();
+			mainLi.removeClass('native');
+			mainLi.eq(iNow).addClass('native');
+		})			
+	})();
+
+	//十大标准
+	(function(){
+		var oLi=$('#biaozhun').find('ul').find('li');
+		var oP=$('#biaozhun').find('ul').find('p');
+		var iNow=0;
+		oLi.click(function(){
+			iNow=$(this).index();
+			if(oP.eq(iNow).css('display')=='block'){
+				$(this).removeClass('native');
+				$(this).find('span').css('visibility','visible');
+			}
+			else{
+				oLi.removeClass('native');
+				oLi.find('span').css('visibility','visible');
+				$(this).addClass('native');
+				$(this).find('span').css('visibility','hidden');	
+			}
+			
+
+			/*
+			iNow=$(this).index();
+			if(oP.eq(iNow).css('display')=='block'){
+				oP.eq(iNow).css('display','none');
+				$(this).find('h2').css({
+					'background-image':'url(img/biaozhun-right.png)',
+					'background-size':'0.12rem 0.24rem'
+				});
+
+				$(this).find('span').css('visibility','visible')
+				
+			}
+			else{
+				oP.eq(iNow).css('display','block');
+				
+				$(this).find('h2').css({
+					'background-image':'url(img/biaozhun-down.png)',
+					'background-size':'0.24rem 0.12rem'
+				})
+
+				$(this).find('span').css('visibility','hidden')
+			}
+			*/
+		})
+	})();
+
+	//步骤
+	/*
+	(function(){
+		var oLi=$('#buzhou').find('.content').find('li');
+		var oP=$('#buzhou').find('.content').find('p');
+		var iNow=0;
+		oLi.click(function(){
+			iNow=$(this).index();
+			oP.removeClass('native');
+			oP.eq(iNow).addClass('native');
+		})
+	})();
+	*/
+})
