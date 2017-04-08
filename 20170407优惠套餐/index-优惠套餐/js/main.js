b57f678e93cd8c481f5ee6ff1ec75474597d6d18
Sup_ <1099982811@qq.com>
2017-04-08 18:07:15 +0800

    静态页面完成
    静态页面完成

--- 20170407优惠套餐/index-优惠套餐/js/main.js
+++ 20170407优惠套餐/index-优惠套餐/js/main.js
@@ -5,5 +5,35 @@ $(document).ready(function(){
 		$('body').css('visibility','visible');
 	})();
 	
+	(function(){
+			var oPic = $("#xiuyang").find('li');
+			var oSpot = $("#xiuyang").find('.spot').find('span')
+			var iNow = 0;
+			var timer=null;
+			
+			clearInterval(timer);
+			setInterval(function(){
+
+			},1000) 
+
+			function fnFade(){
+				oPic.removeClass('active');
+				oPic.eq(iNow).addClass('active');
+
+			}
+
+		/*
+		function fadeIn(id){
+			var oPic = $(id).find('li');
+			var oSpot = $(id).find('.spot').find('span')
+			var iNow = 0;
+			var timer=null;
+
+			setInterval(function(){
+
+			},1000) 
+		}
+		*/
+	})();
 	
 })
