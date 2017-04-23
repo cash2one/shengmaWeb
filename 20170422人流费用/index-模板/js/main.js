1de2522a419c2f2075ef665b89a64d1433bec92e
Sup_ <1099982811@qq.com>
2017-04-23 14:37:31 +0800

    人流费用
    人流费用

--- 20170422人流费用/index-模板/js/main.js
+++ 20170422人流费用/index-模板/js/main.js
@@ -5,5 +5,17 @@ $(document).ready(function(){
 		$('body').css('visibility','visible');
 	})();
 	
-	
+	//套餐服务切换
+	(function(){
+		var menuLi=$('#private').find('.menu').find('li');
+		var contentLi=$('#private').find('.content').find('li');
+		var iNow=0;
+		menuLi.mouseenter(function(){
+			menuLi.removeClass('native');
+			$(this).addClass('native');
+			iNow=$(this).index();
+			contentLi.removeClass('native');
+			contentLi.eq(iNow).addClass('native');
+		})				
+	})();
 })
